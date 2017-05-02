67d79a450072af88b46cb1f3deb19a79d5bda2b0
Sandesh Gade <sandeshgade@gmail.com>
2017-05-02 16:19:06 -0400

    mock server updates

--- server/index.js
+++ server/index.js
@@ -1,29 +1,27 @@
 // Import modules
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 var casual = require('casual');
+var async = require('async');
+var _ = require('underscore');
 
 // Set server parameters
 const MOCK_SERVER_PORT = 3000;
-const DATABASE_WORD_COUNT = 10000;
+const DATABASE_WORD_COUNT = 1000000;
+
+// Enable cors support for cross origin api requests (ONLY FOR DEV)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // Generate fake database;
 var master_list = casual.array_of_words(DATABASE_WORD_COUNT);
 console.log("Database Generated!");
-
-// Register a simple filter function
-function predictionsFilter(value) {
-  var results = [];
-
-  for (i=0; i<master_list.length; i++){
-    if (master_list[i].match(value)){
-      results.push(master_list[i]);
-    }
-  }
-
-  return results.sort().slice(0, 10);
-}
+// console.log(master_list);
 
 // Define API endpoints below >>>>>>>
 
@@ -34,17 +32,35 @@ app.get('/', function(req, res) {
 
 // Endpoint for responding to promises
 app.get('/promise', function(req, res) {
-  res.send({'data':predictionsFilter(req.query.keyword)})
+  console.log("/promise \t -> \t " + req.query.keyword);
+  var result = _.filter(master_list, function(item){ return item.match("^" + req.query.keyword)})
+  res.send({'data': [...new Set(result)].sort().slice(0,10)});
 })
 
 // Endpoint for responding to observables
-app.get('/observables', function(req, res) {
-
+app.get('/observable', function(req, res) {
+  console.log("/observable \t -> \t " + req.query.keyword);
+  var result = _.filter(master_list, function(item){ return item.match("^" + req.query.keyword)})
+  res.send({'data': [...new Set(result)].sort().slice(0,10)});
 })
 
 // websocket endpoint for responding to realtime requests
 io.on('connection', function(socket) {
-  socket.emit('predictions', {data: ["Hello World"]});
+  console.log("Client connected via websockets");
+
+  socket.on('request', function(data) {
+    console.log("CHANNEL: request \t -> \t " + data);
+    var result = _.filter(master_list, function(item){ return item.match("^" + data)});
+    console.log([...new Set(result)].sort().slice(0,10));
+    socket.emit('predictions', {'data': [...new Set(result)].sort().slice(0,10)})
+  });
+
+  socket.on('sync', function(data) {
+    console.log("CHANNEL: sync \t -> \t " + data);
+    var result = _.filter(master_list, function(item){ return item.match("^" + data)});
+    socket.emit('sync_response', {'query': data, 'data' : [...new Set(result)].sort().slice(0,10)});
+  });
+  // socket.emit('predictions', {data: ["Hello World"]});
   // socket.on('')
 })
 
