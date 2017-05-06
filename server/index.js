796f42b1349b83dcc432f66e2f5daaf9065f6849
Sandesh Gade <sandeshgade@gmail.com>
2017-05-05 23:28:15 -0400

    Updated server logic for distributed caching

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 // Import modules
-var app = require('express')();
+const express = require('express');
+var app = express();
+const path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var casual = require('casual');
 var async = require('async');
 var _ = require('underscore');
+var fs = require('fs');
 
 // Set server parameters
 const MOCK_SERVER_PORT = 3000;
-const DATABASE_WORD_COUNT = 1000000;
+const TEST_CHAR_LENGTHS = [31, 25, 20, 15, 10, 5, 1];
+// const DATABASE_WORD_COUNT = 5000000;
 
 // Enable cors support for cross origin api requests (ONLY FOR DEV)
 app.use(function(req, res, next) {
@@ -17,17 +21,57 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Generate fake database;
-var master_list = casual.array_of_words(DATABASE_WORD_COUNT);
-console.log("Database Generated!");
+// var master_list = casual.array_of_words(DATABASE_WORD_COUNT);
+var master_list = fs.readFileSync('words.txt').toString().split("\n");
+
+var lgth = 0;
+var longest;
+var test_keywords = []
+
+for(var i=0; i < master_list.length; i++){
+    if(master_list[i].length > lgth){
+        var lgth = master_list[i].length;
+        longest = master_list[i];
+    }
+}
+
+// Shuffle function
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+
+var shuffle_list = master_list;
+shuffle(shuffle_list);
+
+for(var i=0; i < shuffle_list.length; i++){
+    if(shuffle_list[i].length == TEST_CHAR_LENGTHS[0]){
+      test_keywords.push(shuffle_list[i]);
+      TEST_CHAR_LENGTHS.shift()
+  }
+}
+
+console.log("Keywords for testing: \n***********************");
+console.log(test_keywords);
+console.log("\n****************************");
+
+
+console.log("Database Generated with " + master_list.length + " words!");
+console.log("Longest word in the DB is: " + longest + " with " + longest.length + " letters.");
+
+
 // console.log(master_list);
 
 // Define API endpoints below >>>>>>>
 
 // Default endpoint
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
 // Endpoint for responding to promises
@@ -59,6 +103,7 @@ io.on('connection', function(socket) {
     console.log("CHANNEL: sync \t -> \t " + data);
     var result = _.filter(master_list, function(item){ return item.match("^" + data)});
     socket.emit('sync_response', {'query': data, 'data' : [...new Set(result)].sort().slice(0,10)});
+    socket.broadcast.emit('cache_sync', {'query': data, 'data' : [...new Set(result)].sort().slice(0,10)});
   });
   // socket.emit('predictions', {data: ["Hello World"]});
   // socket.on('')
